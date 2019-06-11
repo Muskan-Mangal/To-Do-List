d7bfbfac0322f5c08c1d77719cca5bfff74d4d38
Muskan Mangal <richa.mangal00@gmail.com>
2019-06-11 20:47:44 +0530

    added ode files

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(()=>{
+
+$(".new_todo_button").on("click",function(){
+  var title=$(".new_todo").val();
+  if(title=='')
+  {
+    window.alert("value required");
+  }
+  else{
+  $(".todo-list").append(
+    $("<li/>").append(
+      $("<div class='list-item'/>").append(
+        $(`<input type=text class='toodo' value='${title}'/>`).on("click",function(){
+          $(this).toggleClass("cut");
+          console.log("here");
+        })
+      ).append(
+          $("<button type='button' class='upbutton'>UP</button>")
+        ).append(
+            $("<button type='button' class='downbutton'>DOWN</button>")).append(
+              $("<button type='button' class='delete'>X</button>").on("click",(event)=>{
+                $(event.currentTarget).parent().parent().fadeOut(()=>{
+                  $(event.currentTarget).parent().parent().remove();
+                });
+
+              }))
+
+    )
+  )
+  $(".new_todo").val("");
+  }
+
+})
+
+$(".delete").on("click",function(){
+
+})
+})
